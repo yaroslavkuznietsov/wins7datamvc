5b0088081ca48c44bfcbb09c7c93485a0e6b4d65
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-05-24 21:08:11 +0300

    Added DatWerkzeugs Views and Controller

--- WinS7Data/WinS7Data.DataModel/RecipeContext.cs
+++ WinS7Data/WinS7Data.DataModel/RecipeContext.cs
@@ -27,5 +27,41 @@ namespace WinS7Data.DataModel
         public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.NameOfInput> NameOfInputs { get; set; }
 
         public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.TimeOfVentil> TimeOfVentils { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.ParameterSchritteClass> ParameterSchritteClasses { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.AbholAchseA1A2Class> AbholAchseA1A2Class { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.AbholSchrittClass> AbholSchrittClasses { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.AktivierungSchritteClass> AktivierungSchritteClasses { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.HEVorpositionSchrittClass> HEVorpositionSchrittClasses { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.HEVorpositionAchseA1A2Class> HEVorpositionAchseA1A2Class { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.Warm1SchrittClass> Warm1SchrittClass { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.Warm1AchseA1A2Class> Warm1AchseA1A2Class { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.Warm1AchseA3Class> Warm1AchseA3Class { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.AchsenStromClass> AchsenStromClasses { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.ZaehlerClass> ZaehlerClasses { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.AktivierungN2GasClass> AktivierungN2GasClass { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.AktivierungSontigesClass> AktivierungSontigesClasses { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.BursterDigiforceKraftWegClass> BursterDigiforceKraftWegClasses { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.BursterDigiforceMinsdestkraftClass> BursterDigiforceMinsdestkraftClasses { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.IRCameraClass> IRCameraClasses { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.DMXCheckClass> DMXCheckClasses { get; set; }
+
+        public System.Data.Entity.DbSet<WinS7Data.DataLibrary.Models.Ausgleichshub> Ausgleichshubs { get; set; }
     }
 }
